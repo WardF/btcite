d6ddc6b6c96ecfa4a2fab465a1f5b14a81f7a60c
Ward Fisher <wfisher@unidata.ucar.edu>
2014-05-05 16:22:59 -0600

    Added initial Vagrantfile, Bootstrap script and .gitignore file.

--- Vagrantfile
+++ Vagrantfile
@@ -11,6 +11,16 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   config.ssh.forward_x11 = "true"
 
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 2048
+    v.cpus = 2
+  end
+
+  config.vm.provider "parallels" do |v|
+    v.memory = 2048
+    v.cpus = 2
+  end
+
   # Every Vagrant virtual environment requires a box to build off of.
   config.vm.define "trusty64",primary: true do |v|
     v.vm.provision :shell, :path => "bootstrap_dev.sh"
--- bootstrap_dev.sh
+++ bootstrap_dev.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
+#####
 # BTCite provision script for development web server.
 # Assume that we're running on Ubuntu.
+#####
+
+#####
+# Update, Install system packages.
+#####
+apt-get update
+apt-get install -y emacs24 ruby1.9.3 sqlite3 libsqlite3-dev nodejs htop
+
+#####
+# Install Ruby-related stuff.
+#####
+
+###
+# Update gems
+###
+
+gem install rubygems-update
+update_rubygems
+
+###
+# Install Gems
+###
+
+gem install rake
+gem install rails
 
 #####
 # Set the proper timezone.
 #####
 echo "US/Mountain" | tee /etc/timezone
 dpkg-reconfigure --frontend noninteractive tzdata
+
