77c30ecc8db506b56c90c5c520d068d819932222
Ward Fisher <wfisher@unidata.ucar.edu>
2014-05-07 15:44:01 -0600

    Added links browser to default install.

--- bootstrap_dev.sh
+++ bootstrap_dev.sh
@@ -8,7 +8,7 @@
 # Update, Install system packages.
 #####
 apt-get update
-apt-get install -y emacs24 ruby1.9.3 sqlite3 libsqlite3-dev nodejs htop git wget zip unzip nmap firefox
+apt-get install -y emacs24 ruby1.9.3 sqlite3 libsqlite3-dev nodejs htop git wget zip unzip nmap firefox links
 
 #####
 # Install Ruby-related stuff.
