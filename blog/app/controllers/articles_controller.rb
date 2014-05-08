57932cb3b30fa5095b2f1b9920596a11a1bd7e32
Ward Fisher <wfisher@unidata.ucar.edu>
2014-05-08 15:39:43 -0600

    Completed section 5.13, moving on to section 6 of the tutorial.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -36,6 +36,14 @@ def edit
   @article = Article.find(params[:id])
 end
 
+def destroy 
+  @article = Article.find(params[:id])
+  @article.destroy
+
+  redirect_to articles_path
+end
+
+
 private
 
 def article_params
