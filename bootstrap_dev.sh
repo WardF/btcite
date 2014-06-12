6a040a52dea4c48f19d2e56a2cc9d908f024de6d
Ward Fisher <ward.fisher@gmail.com>
2014-06-12 15:53:39 -0600

    Changed email address used to configure git on bootstrap.

--- bootstrap_dev.sh
+++ bootstrap_dev.sh
@@ -135,7 +135,7 @@ chmod 775 $SCRIPTFILE
 #####
 
 sudo -i -u vagrant git config --global user.name "Ward Fisher"
-sudo -i -u vagrant git config --global user.email "ward.fisher@gmail.com"
+sudo -i -u vagrant git config --global user.email "wfisher@unidata.ucar.edu"
 sudo -i -u vagrant git config --global push.default simple
 
 #####
